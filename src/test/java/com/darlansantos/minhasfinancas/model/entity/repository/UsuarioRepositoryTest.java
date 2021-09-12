c37783b7bf83086678f9feab38bcaf7246e6a7f1
Darlan Parreiras Santos <darlanparreiras@gmail.com>
2021-09-12 12:57:27 -0300

    Testes unitarios na service

--- src/test/java/com/darlansantos/minhasfinancas/model/entity/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/darlansantos/minhasfinancas/model/entity/repository/UsuarioRepositoryTest.java
@@ -1,25 +1,25 @@
 package com.darlansantos.minhasfinancas.model.entity.repository;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.darlansantos.minhasfinancas.model.entity.Usuario;
 
 @SpringBootTest
-@ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
 @ActiveProfiles("test")
-class UsuarioRepositoryTest {
+public class UsuarioRepositoryTest {
 	
 	@Autowired
 	UsuarioRepository usuarioRepository;
 
 	@Test
-	void deveVerificarAExistenciaDeUmEmail() {	
+	public void deveVerificarAExistenciaDeUmEmail() {	
 		
 		//cenário
 		Usuario usuario = Usuario.builder().nome("usuario").email("usuario@gmail.com").build();
@@ -33,7 +33,7 @@ class UsuarioRepositoryTest {
 	}
 	
 	@Test
-	void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComOEmail() {	
+	public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComOEmail() {	
 		
 		//cenário
 		usuarioRepository.deleteAll();
---
+++ src/test/java/com/darlansantos/minhasfinancas/service/UsuarioServiceTest.java
@@ -0,0 +1,46 @@
+package com.darlansantos.minhasfinancas.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.darlansantos.minhasfinancas.exception.RegraNegocioException;
+import com.darlansantos.minhasfinancas.model.entity.Usuario;
+import com.darlansantos.minhasfinancas.model.entity.repository.UsuarioRepository;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+public class UsuarioServiceTest {
+
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Test(expected = Test.None.class) //Não lança exceção
+	public void deveValidarEmail() {
+		
+		//cenário
+		usuarioRepository.deleteAll();
+		
+		//ação - execução
+		usuarioService.validarEmail("email@email.com");	
+	}
+	
+	@Test(expected = RegraNegocioException.class)
+	public void develancarErroAoValidarEmailQuandoExistirEmailCadastrado() {
+		
+		//cenário
+		Usuario usuario = Usuario.builder().nome("Usuario").email("email@email.com").build();
+		usuarioRepository.save(usuario);
+		
+		//acao
+		usuarioService.validarEmail("email@email.com");
+	}
+
+}
