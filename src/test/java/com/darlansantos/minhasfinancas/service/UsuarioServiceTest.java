23922c1fddc724f27dea5290baf54f0a77ac0143
Darlan Parreiras Santos <darlanparreiras@gmail.com>
2021-09-13 19:13:58 -0300

    Testando Metodo de Autenticacao de Usuario

--- src/test/java/com/darlansantos/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/darlansantos/minhasfinancas/service/UsuarioServiceTest.java
@@ -1,5 +1,8 @@
 package com.darlansantos.minhasfinancas.service;
 
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +12,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.darlansantos.minhasfinancas.exception.RegraNegocioException;
+import com.darlansantos.minhasfinancas.model.entity.Usuario;
 import com.darlansantos.minhasfinancas.model.entity.repository.UsuarioRepository;
 import com.darlansantos.minhasfinancas.service.impl.UsuarioServiceImpl;
 
@@ -24,6 +28,22 @@ public class UsuarioServiceTest {
 	@Before
 	public void setUp() {
 		usuarioService = new UsuarioServiceImpl(usuarioRepository);
+	}
+	
+	@Test(expected = Test.None.class)
+	public void deveAutenticarUmUsuarioComSucesso() {
+		
+		//cenario
+		String email = "email@email.com";
+		String senha = "senha";		
+		Usuario usuario = Usuario.builder().id(1L).email(email).senha(senha).build();		
+		Mockito.when(usuarioRepository.findByEmail(email)).thenReturn(Optional.of(usuario));
+		
+		//acao
+		Usuario result = usuarioService.autenticat(email, senha);
+		
+		//verificacao
+		Assertions.assertThat(result).isNotNull();
 	}
 
 	@Test(expected = Test.None.class) //Não lança exceção
