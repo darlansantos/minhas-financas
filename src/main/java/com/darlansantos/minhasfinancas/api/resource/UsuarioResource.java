11d71d03f285c726f51e0e1c9e64c45734fdce03
Darlan Parreiras Santos <darlanparreiras@gmail.com>
2021-09-14 06:31:18 -0300

    Utilizando o Postman para testar nossa Api Rest

--- src/main/java/com/darlansantos/minhasfinancas/api/resource/UsuarioResource.java
+++ src/main/java/com/darlansantos/minhasfinancas/api/resource/UsuarioResource.java
@@ -1,16 +1,39 @@
 package com.darlansantos.minhasfinancas.api.resource;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.darlansantos.minhasfinancas.api.dto.UsuarioDTO;
+import com.darlansantos.minhasfinancas.model.entity.Usuario;
+import com.darlansantos.minhasfinancas.service.UsuarioService;
+
 @RestController
-@RequestMapping(value = "usuarios")
+@RequestMapping(value = "/api/usuarios")
 public class UsuarioResource {
+		
+	private UsuarioService usuarioService;
 	
-	@GetMapping
-	public String helloWorld() {
-		return "Hello world!";
+	@Autowired
+	public UsuarioResource(UsuarioService usuarioService) {
+		this.usuarioService = usuarioService;
+	}
+
+	@PostMapping
+	public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
+		Usuario usuario = Usuario.builder().nome(dto.getNome()).email(dto.getEmail()).senha(dto.getSenha()).build();
+		
+		try {
+			Usuario usuarioSalvo = usuarioService.salvarUsuario(usuario);
+			return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+		
 	}
 
 }
