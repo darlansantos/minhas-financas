1c104eecab792dd9aa8d9199f68ff38212e0e9e8
Darlan Parreiras Santos <darlanparreiras@gmail.com>
2021-09-15 00:28:34 -0300

    Convertendo o DTO de Lancamentos em Entidade JPA

--- src/main/java/com/darlansantos/minhasfinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/darlansantos/minhasfinancas/api/resource/LancamentoResource.java
@@ -7,21 +7,51 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.darlansantos.minhasfinancas.api.dto.LancamentoDTO;
+import com.darlansantos.minhasfinancas.exception.RegraNegocioException;
+import com.darlansantos.minhasfinancas.model.entity.Lancamento;
+import com.darlansantos.minhasfinancas.model.entity.Usuario;
+import com.darlansantos.minhasfinancas.model.entity.enums.StatusLancamento;
+import com.darlansantos.minhasfinancas.model.entity.enums.TipoLancamento;
 import com.darlansantos.minhasfinancas.service.LancamentoService;
+import com.darlansantos.minhasfinancas.service.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/api/lancamentos")
 public class LancamentoResource {
 
 	private LancamentoService lancamentoService;
+	
+	private UsuarioService usuarioService;
 
-	public LancamentoResource(LancamentoService lancamentoService) {
+	public LancamentoResource(LancamentoService lancamentoService, UsuarioService usuarioService) {
 		this.lancamentoService = lancamentoService;
+		this.usuarioService = usuarioService;
 	}
 	
 	@PostMapping
 	public ResponseEntity<?> salvar (@RequestBody LancamentoDTO dto) {
+		converter(dto);
 		return null;
+		
 	}
-	
+
+	private Lancamento converter(LancamentoDTO dto) {
+		Usuario usuario = usuarioService
+				.obterPorId(dto.getUsuario())
+				.orElseThrow(() -> new RegraNegocioException("Usuário não encontrado para o Id informado."));
+		
+		Lancamento lancamento = Lancamento.builder()
+				.descricao(dto.getDescricao())
+				.ano(dto.getAno())
+				.mes(dto.getMes())
+				.valor(dto.getValor())
+				.tipo(TipoLancamento.valueOf(dto.getTipo()))
+				.status(StatusLancamento.valueOf(dto.getStatus()))
+				.usuario(usuario)
+				.build();
+		
+		return lancamento;
+		
+	}
+		
 }
--- src/main/java/com/darlansantos/minhasfinancas/service/UsuarioService.java
+++ src/main/java/com/darlansantos/minhasfinancas/service/UsuarioService.java
@@ -1,5 +1,7 @@
 package com.darlansantos.minhasfinancas.service;
 
+import java.util.Optional;
+
 import com.darlansantos.minhasfinancas.model.entity.Usuario;
 
 public interface UsuarioService {
@@ -9,5 +11,7 @@ public interface UsuarioService {
 	Usuario salvarUsuario(Usuario usuario);
 	
 	void validarEmail(String email);
+	
+	Optional<Usuario> obterPorId(Long id);
 
 }
